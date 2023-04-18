d98ccfafc4de9d6f738b7d8e8851b2e0b5ecd971
Vusal Abushov <vusalteam@yandex.ru>
2023-04-18 21:30:44 +0300

    sem_4>#2,#4,#5 added,

---
+++ sem_4/#4/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 
+Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24 
+5 -> 120
+ */
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int getCount()
+{
+    int count = 1;
+    for (int i = 1; i <= number; i++)
+    {
+
+        count *= i;
+    }
+    return count;
+}
+Console.WriteLine("Произведение чисел: " + getCount());
---
+++ sem_4/#5/Program.cs
@@ -0,0 +1,13 @@
+﻿/* 
+Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+ */
+
+int[] numArr = new int[8];
+
+for(int i=0;i<numArr.Length;i++){
+    numArr[i] = new Random().Next(0,2);
+} 
+
+    Console.WriteLine(String.Join(", ", numArr));
+
