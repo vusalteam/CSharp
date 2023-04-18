bf3984e120da3fcffefb029362ce6b1de03fb8fc
Vusal Abushov <vusalteam@yandex.ru>
2023-04-18 20:20:29 +0300

    sem_4 first task

---
+++ sem_4/#1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36 
+*/
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int getSum() {
+    int sum = 0;
+    for (int i = 1; i <= number; i++) {
+        sum += i;
+    }
+    return sum;
+}
+    Console.WriteLine("Сумма чисел от 1 до {0} = {1}",number, getSum());
