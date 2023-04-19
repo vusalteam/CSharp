928646c23474f77d760b41551204b622e0b6fe5d
Vusal Abushov <vusalteam@yandex.ru>
2023-04-19 03:07:35 +0300

    home work added

---
+++ sem_4/home-work/#25/Program.cs
@@ -0,0 +1,21 @@
+﻿/* 
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+ */
+
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int getNumsDegree()
+{
+    int result = 1;
+    for (int i = 0; i < b; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
+
+Console.Write("{0} в степени {1} равно: {2}", a, b, getNumsDegree());
---
+++ sem_4/home-work/#27/Program.cs
@@ -0,0 +1,20 @@
+﻿/* 
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int getNumSum(){
+    int sum = 0;
+    int temp = number;
+    while (temp > 0){
+        sum += temp % 10;
+        temp /= 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Сумма цифр в числе: " + getNumSum());
