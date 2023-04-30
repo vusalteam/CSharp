487f8f895812cf775c4cdfadb55a4dc4f68ce3e3
Vusal Abushov <vusalteam@yandex.ru>
2023-04-30 16:04:40 +0300

    Домашнее задание 7 семинара

---
+++ sem_7/home-work/#47/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+Console.WriteLine("Введите количество строк массива");
+string? inputM = Console.ReadLine();
+int m = string.IsNullOrEmpty(inputM) ? 0 : int.Parse(inputM);
+
+Console.WriteLine("Введите количество столбцов массива");
+string? inputN = Console.ReadLine();
+int n = string.IsNullOrEmpty(inputN) ? 0 : int.Parse(inputN);
+
+double[,] arr = new double[m, n];
+for (int i = 0; i < arr.GetLength(0); i++){
+    for (int j = 0; j < arr.GetLength(1); j++){
+        arr[i,j] = Math.Round(new Random().NextDouble()* 20 - 10, 2);
+    }
+}
+Console.WriteLine("Полученный массив");
+for (int i = 0; i < arr.GetLength(0); i++){
+    for (int j = 0; j < arr.GetLength(1); j++)    {
+        Console.Write(arr[i,j] + " ");
+    }
+    Console.WriteLine();
+}
---
+++ sem_7/home-work/#52/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+int[,] randomArray(int rows = 5, int columns = 4)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-99, 100);
+        }
+    }
+    return array;
+}
+void printArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+void getAverage(int[,] array)
+{
+    Console.Write("Среднее арифметическое каждого столбца: ");
+    for (int i = 0; i < array.GetLength(1); i++){
+        double sum = 0;
+        for (int j = 0; j < array.GetLength(0); j++){
+            sum += array[j, i];
+        }
+        double average = Math.Round(sum / array.GetLength(0),1, MidpointRounding.ToNegativeInfinity);
+        Console.Write($"{average}; ");
+    }
+}
+
+
+//int[,] array = new int[,] { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+int[,] array = randomArray(5, 4);
+printArray(array);
+getAverage(array);
