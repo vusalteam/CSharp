f7b39609c820822cd804eece0970c44e1ec98237
Vusal Abushov <vusalteam@yandex.ru>
2023-04-30 14:37:17 +0300

    Домашнее задание 6 семинара

---
+++ sem_6/home-work/#43/Program.cs
@@ -0,0 +1,29 @@
+﻿/* 
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+
+Console.Write("Введите b1: ");
+double b1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите k1: ");
+double k1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите b2: ");
+double b2 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите k2: ");
+double k2 = double.Parse(Console.ReadLine());
+
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны, точки пересечения нет");
+}
+else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+
+    Console.WriteLine("Точка пересечения двух прямых равна: (" + x + "; " + y + ")");
+}
